e05541a9d1c53da7f0ce7205a88b1abbd79dbf93
Javier Farre <jfarre90@gmail.com>
2019-04-02 11:56:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//We need a reducer (rootReducer)
+
+const initialState = {
+    count:0
+}
+
+function rootReducer(state=initialState, action) {
+    // debugger; we can use this to stop the script here for debugging
+    switch (action.type){
+        case 'INCREMENT':
+            var newState = {...state };
+            // let newState = Object.assign ({}, state); This would also work.
+            newState++;
+            return newState;
+            
+        case 'DECREMENT':
+            var newState = {...state };
+            
+            newState--;
+            return newState;
+        default:
+            return state;
+    }
+}
+//We eneed some redux store and initial State
+
+/*global Redux*/
+const store = Redux.createStore(rootReducer);
+
+//We need some way of changing the state
+
+$(document).ready(function(){
+    let currentState = store.getState();
+    $('#counter').text(currentState.count);
+    
+    $('#increment').on('click', function(){
+        store.dispatch({
+            type: 'INCREMENT'
+        });
+        let currentState = store.getState();
+        $('#counter').text(currentState.count);
+    });
+    $('#decrement').on('click', function(){
+        store.dispatch({
+            type: 'DECREMENT'
+        });
+        let currentState = store.getState();
+        $('#counter').text(currentState.count);
+    })
+})
